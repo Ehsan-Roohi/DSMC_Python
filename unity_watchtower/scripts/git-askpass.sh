69069a85303f7da56e9be3af13bcc2057d930afe
Ehsan-Roohi <roohie@umass.edu>
2026-08-06 12:55:23 -0400

    Fix private-repository PAT authentication

--- unity_watchtower/scripts/git-askpass.sh
+++ unity_watchtower/scripts/git-askpass.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 TOKEN_FILE="${UNITY_MONITOR_TOKEN_FILE:-${HOME}/.config/unity-watchtower/github.token}"
 case "${1:-}" in
   *Username*|*username*)
-    printf '%s\n' 'x-access-token'
+    printf '%s\n' "${UNITY_MONITOR_GITHUB_USERNAME:-Ehsan-Roohi}"
     ;;
   *)
     if [[ ! -r "${TOKEN_FILE}" ]]; then
